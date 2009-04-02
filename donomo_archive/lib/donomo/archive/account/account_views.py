2c6e0c825849697d8967f6a73d17bb122a5e3946
alexis <alexis>
2009-04-02 22:48:58 +0000

    linking with real paypal account
    git-svn-id: http://svn.donomo.net/trunk@521 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/lib/donomo/archive/account/account_views.py
+++ donomo_archive/lib/donomo/archive/account/account_views.py
@@ -1,4 +1,5 @@
 from django.http                            import HttpResponse, HttpResponseRedirect
+from django.conf                            import settings
 from django.contrib.auth                    import authenticate, login, logout
 from django.contrib.auth.decorators         import login_required
 from django.shortcuts                       import render_to_response
