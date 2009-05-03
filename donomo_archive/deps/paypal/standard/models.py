0bd22f34d61e830c640d1e866108cca05b633674
deploy <deploy>
2009-05-03 19:49:22 +0000

    debugging
    git-svn-id: http://svn.donomo.net/trunk@542 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/deps/paypal/standard/models.py
+++ donomo_archive/deps/paypal/standard/models.py
@@ -22,7 +22,7 @@ class PayPalStandardBase(models.Model):
     # PENDING_REASON = "address authorization echeck intl multi-currency unilateral upgrade verify other".split()
     # REASON_CODE = "chargeback guarantee buyer_complaint refund other".split()
     # TRANSACTION_ENTITY_CHOICES = "auth reauth order payment".split()
-    
+
     # Transaction and Notification-Related Variables
     business = models.CharField(max_length=127, blank=True, help_text="Email where the money was sent.")
     charset=models.CharField(max_length=32, blank=True)
@@ -35,8 +35,8 @@ class PayPalStandardBase(models.Model):
     test_ipn = models.BooleanField(default=False, blank=True)
     txn_id = models.CharField("Transaction ID", max_length=19, blank=True, help_text="PayPal transaction ID.")
     txn_type = models.CharField("Transaction Type", max_length=128, blank=True, help_text="PayPal transaction type.")
-    verify_sign = models.CharField(max_length=255, blank=True)    
-    
+    verify_sign = models.CharField(max_length=255, blank=True)
+
     # Buyer Information Variables
     address_country = models.CharField(max_length=64, blank=True)
     address_city = models.CharField(max_length=40, blank=True)
@@ -52,12 +52,12 @@ class PayPalStandardBase(models.Model):
     payer_business_name = models.CharField(max_length=127, blank=True)
     payer_email = models.CharField(max_length=127, blank=True)
     payer_id = models.CharField(max_length=13, blank=True)
-    
+
     # Payment Information Variables
     auth_amount = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
     auth_exp = models.CharField(max_length=28, blank=True)
     auth_id = models.CharField(max_length=19, blank=True)
-    auth_status = models.CharField(max_length=9, blank=True) 
+    auth_status = models.CharField(max_length=9, blank=True)
     exchange_rate = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
     invoice = models.CharField(max_length=127, blank=True)
     item_name = models.CharField(max_length=127, blank=True)
@@ -87,13 +87,13 @@ class PayPalStandardBase(models.Model):
     shipping_method = models.CharField(max_length=255, blank=True)
     tax = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
     transaction_entity = models.CharField(max_length=7, blank=True)
-    
+
     # Auction Variables
     auction_buyer_id = models.CharField(max_length=64, blank=True)
     auction_closing_date = models.DateTimeField(blank=True, null=True, help_text="HH:MM:SS DD Mmm YY, YYYY PST")
     auction_multi_item = models.IntegerField(blank=True, default=0, null=True)
     for_auction = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
-        
+
     # Recurring Payments Variables
     amount = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
     amount_per_cycle = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
@@ -103,12 +103,12 @@ class PayPalStandardBase(models.Model):
     payment_cycle= models.CharField(max_length=32, blank=True) #Monthly
     period_type = models.CharField(max_length=32, blank=True)
     product_name = models.CharField(max_length=128, blank=True)
-    product_type= models.CharField(max_length=128, blank=True)    
+    product_type= models.CharField(max_length=128, blank=True)
     profile_status = models.CharField(max_length=32, blank=True)
     recurring_payment_id = models.CharField(max_length=128, blank=True)  # I-FA4XVST722B9
     rp_invoice_id= models.CharField(max_length=127, blank=True)  # 1335-7816-2936-1451
     time_created = models.DateTimeField(blank=True, null=True, help_text="HH:MM:SS DD Mmm YY, YYYY PST")
-    
+
     # Subscription Variables
     amount1 = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
     amount2 = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
@@ -128,22 +128,22 @@ class PayPalStandardBase(models.Model):
     subscr_effective = models.DateTimeField(blank=True, null=True, help_text="HH:MM:SS DD Mmm YY, YYYY PST")
     subscr_id = models.CharField(max_length=19, blank=True)
     username = models.CharField(max_length=64, blank=True)
-    
+
     # Dispute Resolution Variables
     case_creation_date = models.DateTimeField(blank=True, null=True, help_text="HH:MM:SS DD Mmm YY, YYYY PST")
     case_id = models.CharField(max_length=14, blank=True)
     case_type = models.CharField(max_length=24, blank=True)
-    
+
     # Variables not categorized
-    receipt_id= models.CharField(max_length=64, blank=True)  # 1335-7816-2936-1451 
+    receipt_id= models.CharField(max_length=64, blank=True)  # 1335-7816-2936-1451
     currency_code = models.CharField(max_length=32, default="USD", blank=True)
     handling_amount = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
     transaction_subject = models.CharField(max_length=255, blank=True)
 
     # Mass Pay Variables (Not Implemented, needs a separate model, for each transaction x)
-    # fraud_managment_pending_filters_x = models.CharField(max_length=255, blank=True) 
-    # option_selection1_x = models.CharField(max_length=200, blank=True) 
-    # option_selection2_x = models.CharField(max_length=200, blank=True) 
+    # fraud_managment_pending_filters_x = models.CharField(max_length=255, blank=True)
+    # option_selection1_x = models.CharField(max_length=200, blank=True)
+    # option_selection2_x = models.CharField(max_length=200, blank=True)
     # masspay_txn_id_x = models.CharField(max_length=19, blank=True)
     # mc_currency_x = models.CharField(max_length=32, default="USD", blank=True)
     # mc_fee_x = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
@@ -156,7 +156,7 @@ class PayPalStandardBase(models.Model):
     # status_x = models.CharField(max_length=9, blank=True)
     # unique_id_x = models.CharField(max_length=13, blank=True)
 
-    # Non-PayPal Variables - full IPN/PDT query and time fields.    
+    # Non-PayPal Variables - full IPN/PDT query and time fields.
     ipaddress = models.IPAddressField(blank=True)
     flag = models.BooleanField(default=False, blank=True)
     flag_code = models.CharField(max_length=16, blank=True)
@@ -165,7 +165,7 @@ class PayPalStandardBase(models.Model):
     response = models.TextField(blank=True)  # What we got back.
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         abstract = True
 
@@ -174,37 +174,37 @@ class PayPalStandardBase(models.Model):
             return self.FORMAT % ("Transaction", self.txn_id)
         else:
             return self.FORMAT % ("Recurring", self.recurring_payment_id)
-        
+
     def is_transaction(self):
         return len(self.txn_id) > 0
 
     def is_recurring(self):
         return len(self.recurring_payment_id) > 0
-    
+
     def is_subscription_cancellation(self):
         return self.txn_type == "subscr_cancel"
-    
+
     def is_subscription_end_of_term(self):
         return self.txn_type == "subscr_eot"
-    
+
     def is_subscription_modified(self):
         return self.txn_type == "subscr_modify"
-    
+
     def is_subscription_signup(self):
         return self.txn_type == "subscr_signup"
-    
+
     def set_flag(self, info, code=None):
         """Sets a flag on the transaction and also sets a reason."""
         self.flag = True
         self.flag_info += info
         if code is not None:
             self.flag_code = code
-        
+
     def verify(self, item_check_callable=None):
         """
         Verifies an IPN and a PDT.
         Checks for obvious signs of weirdness in the payment and flags appropriately.
-        
+
         Provide a callable that takes an instance of this class as a parameter and returns
         a tuple (True, None) if the item is valid. Should return (False, "reason") if the
         item isn't valid. This function should check that `mc_gross`, `mc_currency`
@@ -213,14 +213,25 @@ class PayPalStandardBase(models.Model):
         """
         logging.info('in verify')
         self._postback()
-        self._verify_postback()  
+
+        logging.info('after postback')
+        self._verify_postback()
+
+        logging.info('after verifypostback')
+
         if not self.flag:
+            logging.info('not flag')
             if self.is_transaction():
-                if self.payment_status != "Completed":
+                logging.info('tx status=%s' % str(self.payment_status))
+
+                if self.payment_status != "Completed" and self.payment_status != "Pending":
+                    logging.error("Invalid payment_status (%s). " % self.payment_status)
                     self.set_flag("Invalid payment_status (%s). " % self.payment_status)
                 if duplicate_txn_id(self):
+                    logging.error("Duplicate transaction ID (%s). " % self.txn_id)
                     self.set_flag("Duplicate transaction ID (%s). " % self.txn_id)
                 if self.receiver_email != RECEIVER_EMAIL:
+                    logging.error("Invalid receiver_email (%s). Need %s " % (self.receiver_email, RECEIVER_EMAIL))
                     self.set_flag("Invalid receiver_email (%s). " % self.receiver_email)
                 if callable(item_check_callable):
                     flag, reason = item_check_callable(self)
@@ -229,10 +240,15 @@ class PayPalStandardBase(models.Model):
             else:
                 # ### To-Do: Need to run a different series of checks on recurring payments.
                 pass
-        
+
+        logging.info('before save')
         self.save()
         logging.info('about to send signals')
-        self.send_signals()
+        try:
+            self.send_signals()
+        except Exception, e:
+            logging.error(e)
+
         logging.info('DONE sending signals')
 
     def verify_secret(self, form_instance, secret):
@@ -257,11 +273,11 @@ class PayPalStandardBase(models.Model):
     def send_signals(self):
         """After a transaction is completed use this to send success/fail signals"""
         raise NotImplementedError
-        
+
     def _postback(self):
         """Perform postback to PayPal and store the response in self.response."""
         raise NotImplementedError
-        
+
     def _verify_postback(self):
         """Check self.response is valid andcall self.set_flag if there is an error."""
         raise NotImplementedError
