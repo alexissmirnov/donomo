6a652f06b8b46cd6c7ddc624c6e0d8ae2fc14355
deploy <deploy>
2009-05-30 17:52:21 +0000

    relocate external python libraries to live in lib
    git-svn-id: http://svn.donomo.net/trunk@548 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/bin/env.sh
+++ donomo_archive/bin/env.sh
@@ -1,5 +1,5 @@
 
-export PYTHONPATH=${DONOMO_HOME:?}/deps:$DONOMO_HOME/lib${PYTHONPATH:+:$PYTHONPATH}
+export PYTHONPATH=${DONOMO_HOME:?}/lib${PYTHONPATH:+:$PYTHONPATH}
 export DJANGO_SETTINGS_MODULE=donomo.settings
 
 META_DATA=/var/spool/ec2/meta-data.sh
@@ -8,7 +8,7 @@ AWS_OPTS=/root/.donomo/aws.sh
 
 [[ -f $META_DATA ]] && source $META_DATA
 [[ -f $USER_DATA ]] && source $USER_DATA
-[[ -f $AWS_OPTS ]] && source $AWS_OPTS
+[[ -f $AWS_OPTS  ]] && source $AWS_OPTS
 
 PAYPAL_SANDBOX=1
 
