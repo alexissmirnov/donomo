23ebf010e151f21ab4c33476881f8ce4f4f9bede
alexis <alexis>
2009-09-07 17:32:46 +0000

    trial feature
    git-svn-id: http://svn.donomo.net/trunk@571 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/lib/registration/views.py
+++ donomo_archive/lib/registration/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from registration.forms import RegistrationForm
+from registration.forms import RegistrationFormUniqueEmail
 from registration.models import RegistrationProfile
 
 
@@ -74,7 +74,7 @@ def activate(request, activation_key,
 
 
 def register(request, success_url=None,
-             form_class=RegistrationForm, profile_callback=None,
+             form_class=RegistrationFormUniqueEmail, profile_callback=None,
              template_name='registration/registration_form.html',
              extra_context=None):
     """
@@ -160,5 +160,5 @@ def register(request, success_url=None,
     for key, value in extra_context.items():
         context[key] = callable(value) and value() or value
     return render_to_response(template_name,
-                              { 'form': form },
+                              locals(),
                               context_instance=context)
