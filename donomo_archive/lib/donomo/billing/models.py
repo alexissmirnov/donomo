7d1e3a2b2296a7c45850b1f60b41f6f41b1dc01f
deploy <deploy>
2009-11-18 23:34:16 +0000

    200 free pages for new accoutn signup
    git-svn-id: http://svn.donomo.net/trunk@593 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/lib/donomo/billing/models.py
+++ donomo_archive/lib/donomo/billing/models.py
@@ -1,7 +1,7 @@
 from django.db                           import models
 from django.contrib.auth.models          import User
 
-PRICING_PLANS = {'payg1' : [29.99, 1000, 30000], 
+PRICING_PLANS = {'payg1' : [29.99, 1000, 30000],
                   'payg2' : [99.95, 4000, 120000],
                   'plus' : [30.00, 1500, 45000],
                   'pro' : [90.00, 5000, 150000],
@@ -13,11 +13,11 @@ def get_remaining_credit(user, billable_event = 'ocr.ocropus.page'):
     """
     Returns the amount of billable events that the account can perform,
     based on available credit.
-    
+
     Eg. if a billable event is OCRing one page with orcopus, the return
     is a count of pages that a user can process with remaining account
     balance.
-    
+
     For inactive (trial) account, return 1 meaning "have enough credit to
     perform one billable event"
     """
@@ -27,13 +27,13 @@ def get_remaining_credit(user, billable_event = 'ocr.ocropus.page'):
     else:
         # For trial account, return enough credits to perform one billable event
         return 1
-    
+
 def change(user, charge):
     """
     Charges a user a given number of credits.
     Returns True is the charge was successful.
     """
-    
+
     # TODO: wrap in a transaction
     account = Account.objects.get(user = user)
     return charge_account(account, charge)
@@ -42,11 +42,11 @@ def charge_account(account, charge):
     """
     Charges an account object a given number of credits.
     Returns True is the charge was successful.
-    
+
     Charge is measured in "credits". Balance is measured in credits
     """
     balance = account.balance - charge
-    
+
     if balance > 0:
         account.balance = balance
         account.save()
@@ -72,11 +72,11 @@ def refill_account( account, usd, purchace_plan = None):
             purchace_plan = 'max'
         else:
             return
-    
+
     gross, pages, credit = PRICING_PLANS[purchace_plan]
     account.balance = account.balance + credit
     account.save()
-    
+
 def process_billable_event(user, event_name):
     """
     This function will perform a charge that corresponds to the
@@ -96,14 +96,14 @@ class Invoice(models.Model):
 
 class Account(models.Model):
     PRODUCT_CREDIT_CARGE = {'ocr.ocropus.page': 30}
-    BALANCE_ON_CREATION = 5.00
+    BALANCE_ON_CREATION = 6000.00
 
-    
+
     user = models.ForeignKey(
         User,
         unique = True,
         null   = False)
     balance = models.IntegerField()
-    
+
     def prepaid_product_ocr(self):
         return max(0, self.balance) / float(Account.PRODUCT_CREDIT_CARGE['ocr.ocropus.page'])
