a7f26b19af0a376084e113ab95dcd8dd3d519aaa
alexis <alexis>
2010-09-25 01:51:09 +0000

    creating message metadata and assets for every mail part, including test/plain and text/html
    git-svn-id: http://svn.donomo.net/trunk@611 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/lib/donomo/archive/service/mail_parser.py
+++ donomo_archive/lib/donomo/archive/service/mail_parser.py
@@ -15,20 +15,174 @@ from django.db                import transaction
 from django.db.models         import ObjectDoesNotExist
 from donomo.archive           import operations
 from donomo.archive.models    import *
+from donomo.archive.utils.misc import extract_text_from_html
 from logging                  import getLogger
 
+import datetime
 import email
-import re
+import email.utils
+import email.header
 import os
+import re
+import time
 
 MODULE_NAME     = os.path.splitext(os.path.basename(__file__))[0]
 logging         = getLogger(MODULE_NAME)
 DEFAULT_INPUTS  = [ AssetClass.UPLOAD ]
-DEFAULT_OUTPUTS = [ AssetClass.UPLOAD ]
+DEFAULT_OUTPUTS = [ AssetClass.MESSAGE_PART ]
 DEFAULT_ACCEPTED_MIME_TYPES = [ MimeType.MAIL ]
+
+#TODO: replace this by django's version
+def is_email(s):
+    re_email = re.compile(r"(?:^|\s)[-a-z0-9_.]+@(?:[-a-z0-9]+\.)+[a-z]{2,6}(?:\s|$)",re.IGNORECASE)
+    return len(re_email.findall(s)) > 0
+
+def get_or_create_adddress_and_contact(name, address_string, owner):
+    if not address_string:
+        return None, None
+    
+    name = header_to_unicode(name) or ''
+    
+    address = None
+    contact = None
+    try:
+        address = Address.objects.get(email = address_string,
+                                      owner = owner)
+        # found the email we already have
+        # then try to fix the contact's name 
+        # do it only if needed i.e. when the name looks like an email address
+        # and only when we have a better name to supply i.e. name isn't empty
+        contact = address.contact
+        
+        logging.info('found address %s with the contact %s' % (address, contact.pk));
+        if name and name != '' and is_email(contact.name):
+            contact.name = name
+            contact.save()
+    except Address.DoesNotExist, e:
+        address = Address(email = address_string, owner = owner)
+        
+        # did this email came without a name, then set the name to email
+        if name == '':
+            name = address_string
+        
+        # look up the existing contact or create a new one
+        # TODO: handle the case of two people having the same name
+        contact, created = Contact.objects.get_or_create(name = name, 
+                                                        owner = owner)
+        address.contact = contact
+        address.save()
+        logging.info('created address %s with the contact %s (created? %d)' % (address, contact, created));
+
+            
+    return address, contact
+
+# Assume the tags are included in the filename
+def get_or_create_tag_from_asset(mail_asset):
+    file_name = mail_asset.orig_file_name
+    # FIXME
+    # I SOOO suck at regex
+    # trying to extract the name of the label from 
+    # comma-searated list of key/value pairs
+    # ...LABEL=label name,FLAGS=....
+    label = file_name[file_name.find('LABEL=') + len('LABEL='):]
+    label = label[:label.find(',')]
+    if label.find('[Gmail]') == 0:
+        tag_class = Tag.MAIL_GMAIL_LABEL
+    elif label == 'INBOX':
+        tag_class = Tag.MAIL_INBOX
+    else:
+        tag_class = Tag.MAIL_IMAP_FOLDER
+    tag, created = Tag.objects.get_or_create(owner = mail_asset.owner,
+                                             label = label, 
+                                             tag_class = tag_class)
+    return tag
+
+def get_mailbox_address_from_asset(mail_asset):
+    file_name = mail_asset.orig_file_name
+
+    # FIXME: use regex
+    account = file_name[file_name.find('ACCOUNT=') + len('ACCOUNT='):]
+    email = account.split(',')[0]
+    return get_or_create_adddress_and_contact(mail_asset.owner.username,
+                                              email, 
+                                              mail_asset.owner)[0]
+    
+def get_read_flag_from_asset(mail_asset):
+    file_name = mail_asset.orig_file_name
+    # FIXME
+    # I SOOO suck at regex
+    # trying to extract the name of the label from 
+    # comma-searated list of key/value pairs
+    # ...LABEL=label name,FLAGS=....
+    flags = file_name[file_name.find('FLAGS=') + len('FLAGS='):]
+    flags = flags.split(',')
+    
+    return 'S' in flags
+
+
+##############################################################################
+def generate_conversation_summary( body, type, summary_length = 1000 ):
+    if type == MimeType.HTML:
+        text = extract_text_from_html( body )
+    else:
+        text = body
+        
+    # FIXME: regex?
+    # strip consecutive spaces and newlines while
+    # avoiding gluing words together
+    
+    words = text.split(' ')
+    summary = StringIO()
+    
+    for w in words:
+        if len(summary.getvalue()) > summary_length:
+            break;
+        
+        if len(w) and w[0] != '\n':
+            summary.write( w.strip('\n') + ' ')
+            
+    return summary.getvalue()
 
 ##############################################################################
 
+def classify_conversation(conversation):
+    """
+    Assumes 'INBOX' folder. If a conversation is tagged ONLY as 'INBOX' this
+    means it is not classified.
+    
+    Remove from INBOX if it is already tagged with something else.
+    """
+    
+    try:
+        inbox = conversation.tags.get(label = 'INBOX')
+    except Tag.DoesNotExist, e:
+        # already classified. fine
+        return
+    
+    # So INBOX is one folder. Is this conversation already classified
+    # in some other folders?
+    count = conversation.tags.filter(tag_class = Tag.MAIL_IMAP_FOLDER).count()
+    if count > 1:
+        # Yes it is, remove the inbox tag then
+        conversation.tags.remove(inbox)
+        conversation.save()
+        
+    
+def header_to_unicode(raw_header):
+    """
+    Decode a message header value and converts it to unicode string
+    """
+    if not raw_header:
+        return None
+    
+    header = email.header.decode_header(raw_header)[0]
+    if header[1] and header[1].lower() == 'iso-8859-1':
+        return header[0].decode('iso-8859-1').encode('utf-8')
+    else:
+        return unicode(header[0])
+    
+#TODO generate subject for conversations with no-subject emails
+#first few words of the body
 def handle_work_item( processor, work_item ):
 
     """ Pick up an uploaded email and break out each attachment we understand
@@ -39,40 +193,143 @@ def handle_work_item( processor, work_item ):
     asset_list   = []
     parent_asset = work_item['Asset-Instance']
     upload_class = manager(AssetClass).get( name = AssetClass.UPLOAD )
+    message_part_class, created = manager(AssetClass).get_or_create( 
+                                        name = AssetClass.MESSAGE_PART )
     counter      = 0
+    
+#    logging.info('*******************************************')
+#    logging.info(file(work_item['Local-Path']).read())
+#    logging.info('*******************************************')
+    # create contacts, message, conversation
+    raw_message = email.message_from_file(file(work_item['Local-Path']))
+    
+    # create the record for the sender
+    sender = header_to_unicode(raw_message['From'])
+    subject = header_to_unicode(raw_message['Subject'])
+    in_reply_to = raw_message['In-Reply-To']
+    message_date = raw_message['Date']
+    message_id = raw_message['Message-ID']
+    
+    sender_name, sender_address_string = email.utils.parseaddr(sender)
+    sender_name = sender_name.strip('\'')
+    
+    sender_address, sender_contact = get_or_create_adddress_and_contact(
+                                        sender_name, 
+                                        sender_address_string,
+                                        parent_asset.owner)
+        
+        
+    # create a conversation or get one from previously registered email
+    conversation = None
+    in_reply_to_message = None
+    try:
+        in_reply_to_message = Message.objects.get( message_id = in_reply_to )
+        conversation = in_reply_to_message.conversation
+    except Message.DoesNotExist, e:
+        conversation, created = Conversation.objects.get_or_create(
+                    owner = parent_asset.owner,
+                    subject = (subject or '-'). #FIXME regex!
+                            strip('Re: ').strip('RE: ').strip('re: '),
+                    defaults = {'key_participant': sender_address}) 
 
-    for part in email.message_from_file(work_item['Local-Path']).walk():
+    # tag the conversation
+    tag = get_or_create_tag_from_asset(parent_asset)
+    if tag:
+        conversation.tags.add(tag)
+        conversation.save()
+    
+    # get the mailbox account address from asset filename
+    # this isn't the same thing as "To:" address because the mail
+    # may be sent as BCC and arrive to one of many mailboxes
+    # associated to a single account
+    mailbox_address = get_mailbox_address_from_asset(parent_asset)
+    
+    # Date field sometimes could be empty
+    if message_date:
+        message_date = datetime.datetime.fromtimestamp(time.mktime(
+                                        email.utils.parsedate(
+                                                message_date)))
+    else:
+        message_date = datetime.datetime.now()
+        
+    message = Message(owner = parent_asset.owner,
+                      subject = subject,
+                      message_id = message_id,
+                      mailbox_address = mailbox_address,
+                      date = message_date,
+                      reply_to = in_reply_to_message,
+                      conversation = conversation,
+                      sender_address = sender_address)
+
+    if raw_message.get_all('to'):              
+        for a in email.utils.getaddresses(raw_message.get_all('to')):
+            address, contact = get_or_create_adddress_and_contact(
+                                                          a[0].strip('\''), 
+                                                          a[1],
+                                                          parent_asset.owner)
+            message.to_addresses.add(address)
+    
+    if raw_message.get_all('cc'):              
+        for a in email.utils.getaddresses(raw_message.get_all('cc')):
+            address, contact = get_or_create_adddress_and_contact(
+                                                          a[0].strip('\''), 
+                                                          a[1],
+                                                          parent_asset.owner)
+            message.to_addresses.add(address)
+    
+    message.save()
+                      
+    classify_conversation(conversation)
+
+    for part in raw_message.walk():
+        logging.info('part.get_content_type()=%s' % part.get_content_type())
         counter += 1
 
         mime_type = manager(MimeType).filter(name = part.get_content_type())
+        
         if len(mime_type) == 0:
+            logging.error('%s not found in %s. Will not create asset for this part of the message' % (part.get_content_type(), manager(MimeType).all()))
             continue
 
         mime_type = mime_type[0]
 
-        if not upload_class.has_consumers(mime_type):
-            continue
+        if not upload_class.has_consumers(part.get_content_type()):
+            logging.info('%s has NO consumers for %s(%s)' % (upload_class, mime_type.name, part.get_content_type()))
+            #continue
 
-        file_name = part.get_filename or 'part-%04d.%s' % (
+        file_name = part.get_filename() or 'part-%04d%s' % (
             counter,
             mime_type.extension )
 
-        logging.debug(
+        logging.info(
             'MessagePart<filename=%s, mime_type=%s>' %(
                 file_name,
                 mime_type ))
+
+        # Added decode call to avoid errors like
+        # UnicodeDecodeError: 'ascii' codec can't decode 
+        # byte 0x91 in position 3474: ordinal not in range(128)
+        #TODO: figure out why 
+        payload = part.get_payload(decode=True).decode('utf8', 'ignore')
+
+        #TODO update the summary with latest message
+        if conversation.summary == '':
+            conversation.summary = generate_conversation_summary(payload, mime_type)
+            conversation.save()
 
         asset_list.append(
             operations.create_asset_from_stream(
                 data_stream  = StringIO(part.get_payload(decode=True)),
                 owner        = parent_asset.owner,
                 producer     = processor,
-                asset_class  = upload_class,
+                asset_class  = message_part_class,
                 file_name    = file_name,
-                parent_asset = parent_asset,
+                parent       = parent_asset,
                 child_number = counter,
-                mime_type    = mime_type ))
-
+                mime_type    = mime_type,
+                related_message = message ))
+        
+        
     return asset_list
 
 ##############################################################################
@@ -81,13 +338,14 @@ fax_gateway_regex = re.compile(
     r'\s*Caller-ID: (\d{3})\D*(\d{3})\D*(\d{4})\s*$',
     re.IGNORECASE )
 
+
 def _get_owner( email_file ):
     """ Check the subject for the fax gateway's regex and check the email
         address of the sender to see if we can decude a user.
 
     """
 
-    message = email.message_from_file(email_file)
+    message = email.message_from_file(file(email_file))
 
     match = fax_gateway_regex.search(message['subject'])
     if match:
@@ -102,12 +360,12 @@ def _get_owner( email_file ):
             logging.info('No matching user found for %s' % fax_number)
 
     return User.objects.get(
-        email = email.utils.parseaddr(message['from']) [1] )
+        email = email.utils.parseaddr(message['to']) [1] )
 
 ##############################################################################
 
 @transaction.commit_on_success
-def process_mail(processor, local_path):
+def process_mail(owner, processor, local_path):
     """ Process an email message, which is assumed to have been stored on
         the local file system at the path given by local_path.
     """
@@ -115,10 +373,10 @@ def process_mail(processor, local_path):
     return [
         operations.create_asset_from_file(
             file_name    = local_path,
-            owner        = _get_owner(local_path),
+            owner        = owner,
             producer     = processor,
             asset_class  = AssetClass.UPLOAD,
-            parent_asset = None,
+#            parent_asset = None,
             child_number = 0,
             mime_type    = MimeType.MAIL ),
         ]
@@ -134,11 +392,18 @@ def main():
         '-d',
         '--delete',
         action  ='store_true',
-        default = 'False',
+        default = False,
         help    = 'Delete each input file after successfully queing it' )
+    parser.add_option(
+        '-u',
+        '--user')
 
     options, file_name_list = parser.parse_args()
-
+    
+    logging.info(options)
+    
+    owner = User.objects.get(username=options.user)
+    
     processor = operations.initialize_processor(
         MODULE_NAME,
         DEFAULT_INPUTS,
@@ -149,7 +414,7 @@ def main():
     #   -> no exception type given
     for file_name in file_name_list:
         try:
-            new_items = process_mail(processor, file_name)
+            new_items = process_mail(owner, processor, file_name)
             operations.publish_work_item(*new_items)
         except:
             logging.exception("Failed to process %s" % file_name )
