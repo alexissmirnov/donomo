043b8988b2312f2e2628f8c70f89bb2fcf5dcfa3
deploy <deploy>
2010-09-27 17:49:00 +0000

    removing (debugging) limitation on the number of messages offlineimap pulls from the imap server. now getting all messages
    git-svn-id: http://svn.donomo.net/trunk@626 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/lib/donomo/archive/utils/sync.py
+++ donomo_archive/lib/donomo/archive/utils/sync.py
@@ -1,5 +1,7 @@
+from __future__ import with_statement
 from django.conf                     import settings
 from donomo.archive                  import models
+
 import ConfigParser
 import StringIO
 import os
@@ -8,25 +10,25 @@ def update_sync_config(account):
     """
     Updates the configuration of the module responsible for
     syncing content with the remote service.
-    
+
     Currently only supports gmail
     """
     if account.account_class.name != models.AccountClass.EMAIL_GMAIL:
         return False
-    
+
     config = ConfigParser.RawConfigParser()
     config.read(settings.OFFLINEIMAP_CONFIG_FILE)
-    
+
     account_name = '%s.%s' % (account.owner.username, account.name)
     accounts = StringIO.StringIO()
-    
+
     existing_accounts = config.get('general', 'accounts')
     if len(existing_accounts) > 0:
+        accounts.write('%s,' % (account_name) )
         accounts.write(config.get('general', 'accounts'))
-        accounts.write(',%s' % (account_name) )
     else:
         accounts.write('%s' % (account_name) )
-        
+
     config.set('general', 'accounts', accounts.getvalue())
 
     section_name = 'Account %s' % account_name
@@ -34,19 +36,20 @@ def update_sync_config(account):
     config.set(section_name, 'localrepository', '%s.local' % account_name)
     config.set(section_name, 'remoterepository', '%s.remote' % account_name)
     config.set(section_name, 'username', account.owner.username)
-    
+    config.set(section_name, 'autorefresh', '1')
+
     # When you are starting to sync an already existing account yuo can tell offlineimap
     # to sync messages from only the last x days. When you do this messages older than x
     # days will be completely ignored. This can be useful for importing existing accounts
     # when you do not want to download large amounts of archive email.
-    
+
     # Messages older than maxage days will not be synced, their flags will
     # not be changed, they will not be deleted etc. For offlineimap it will be like these
     # messages do not exist. This will perform an IMAP search in the case of IMAP or Gmail
     # and therefor requires that the server support server side searching. This will
     # calculate the earliest day that would be included in the search and include all
     # messages from that day until today. e.g. maxage = 3 to sync only the last 3 days mail
-    config.set(section_name, 'maxage', '10') 
+    # config.set(section_name, 'maxage', '10')
 
     section_name = 'Repository %s.local' % account_name
     config.add_section(section_name)
@@ -62,11 +65,11 @@ def update_sync_config(account):
     # Writing our configuration file
     with open(settings.OFFLINEIMAP_CONFIG_FILE, 'wb') as configfile:
         config.write(configfile)
-    
+
     #reload_offlineimap()
-    
+
     return True
 
 def reload_offlineimap():
     status = os.system('service offlineimap restart')
-    
+
