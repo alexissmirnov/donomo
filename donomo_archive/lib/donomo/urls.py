b9349f92fed9b18cda161f41994f97637c215f98
alexis <alexis>
2010-09-30 01:56:40 +0000

    showing dir index on /media/ in debug deployment
    git-svn-id: http://svn.donomo.net/trunk@629 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/lib/donomo/urls.py
+++ donomo_archive/lib/donomo/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns(
     # sproutcore requires /static prefix for its application
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'show_indexes': True, 'document_root' : settings.MEDIA_ROOT + '/static/'}),
     #(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT + '/sc/build/static/'}),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'show_indexes': True, 'document_root': settings.MEDIA_ROOT }),
     (r'^admin/(.*)', admin.site.root),
     (r'^', include('donomo.archive.urls')),
 )
