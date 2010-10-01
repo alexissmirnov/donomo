1e7ec39000e97f1b4ecdde19725095b244d72335
alexis <alexis>
2010-10-01 01:06:07 +0000

    ui cleanup
    git-svn-id: http://svn.donomo.net/trunk@634 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- inzen/nav/apps/nav/resources/flows_page.js
+++ inzen/nav/apps/nav/resources/flows_page.js
@@ -6,33 +6,33 @@ require ('views/conversation_view');
 App.flowsPage = SC.Page.design({
 	mainPane: SC.MainPane.design({
 		classesName: 'flow-pane'.w(),
-		childViews: 'topbar flows conversation'.w(),
+		childViews: 'flows conversation'.w(),
     
-	    topbar: SC.View.design({
-	        classNames: 'topbar'.w(),
-	        layout: { height: 45, left:0, top:0, right:0 },
-	        childViews: 'logo settings'.w(),
-	        
-	        
-	        settings: SC.ButtonView.design({
-	            layout: {top:7,right:20,height:31,width:47},
-	            title: 'settings'
-	        }),
-	        
-	        logo: SC.ImageView.design({
-	            canLoadInBackground: YES,
-		          layout: {top:2, left:2, width:40, height:40},
-		          value: '/media/img/inzen-logo-40.png'
-		    })
-	    }),
+//	    topbar: SC.View.design({
+//	        classNames: 'topbar'.w(),
+//	        layout: { height: 45, left:0, top:0, right:0 },
+//	        childViews: 'logo settings'.w(),
+//	        
+//	        
+//	        settings: SC.ButtonView.design({
+//	            layout: {top:7,right:20,height:31,width:47},
+//	            title: 'settings'
+//	        }),
+//	        
+//	        logo: SC.ImageView.design({
+//	            canLoadInBackground: YES,
+//		          layout: {top:2, left:2, width:40, height:40},
+//		          value: '/media/img/inzen-logo-40.png'
+//		    })
+//	    }),
 	    
 	    flows: App.DashboardScrollView.design({
-	    	layout: {top:45,right:0,bottom:0,left:0}
+	    	layout: {top:0,right:0,bottom:0,left:0}
 	    }),
 	    
 	    conversation: App.ConversationPanel.design(SC.Animatable, {
 	    	classNames: 'conversation-panel'.w(),
-	    	layout: {top: 55, bottom: 10, right: 0, width: 0 }, // initially hidden (width: 0)
+	    	layout: {top: 5, bottom: 5, right: 0, width: 0 }, // initially hidden (width: 0)
 	    	transitions: {
 	    		 width: { duration: .25, timing: SC.Animatable.TRANSITION_EASE_IN_OUT } // with timing curve
 	    	},
