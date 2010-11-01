9a2bd4688cb4c90dc3b05f33eeee51b807f387d5
alexis <alexis>
2010-11-01 02:52:39 +0000

    moving offlineimap.pid filename into settings
    git-svn-id: http://svn.donomo.net/trunk@682 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/lib/donomo/archive/utils/sync.py
+++ donomo_archive/lib/donomo/archive/utils/sync.py
@@ -9,7 +9,7 @@ import signal
 def update_sync_config():
     """ Signal the offlineimap daemon to reload its configuration.
     """
-    with open('/var/run/donomo/offlineimap.pid') as pid_file:
+    with open(settings.OFFLINEIMAP_PID_FILE) as pid_file:
         pid = int(pid_file.read())
 
     os.kill(pid, signal.SIGHUP)
