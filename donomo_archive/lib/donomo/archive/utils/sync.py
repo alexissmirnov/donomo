3722391e0a563feef77dd91dfbaa1298d34c6a8a
alexis <alexis>
2010-11-02 20:12:59 +0000

    won't crash in case offlineimap process we're trying to restart isn't running
    git-svn-id: http://svn.donomo.net/trunk@690 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- donomo_archive/lib/donomo/archive/utils/sync.py
+++ donomo_archive/lib/donomo/archive/utils/sync.py
@@ -5,6 +5,10 @@ from donomo.archive                  import models
 import ConfigParser
 import os
 import signal
+import logging
+
+logging = logging.getLogger('sync')
+
 
 def update_sync_config():
     """ Signal the offlineimap daemon to reload its configuration.
@@ -12,6 +16,10 @@ def update_sync_config():
     with open(settings.OFFLINEIMAP_PID_FILE) as pid_file:
         pid = int(pid_file.read())
 
-    os.kill(pid, signal.SIGHUP)
-
+    try:
+        os.kill(pid, signal.SIGHUP)
+    except OSError,e:
+        logging.warning(e)
+        pass
+    
     return True
